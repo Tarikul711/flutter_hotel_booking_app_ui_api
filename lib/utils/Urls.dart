590cd1195c444709c6d71f019150db7b959e6d83
tarikulstack <tarikul@topofstacksoftware.com>
2020-04-27 08:32:07 +0600

    Change the application basic stracture

--- lib/utils/Urls.dart
+++ lib/utils/Urls.dart
@@ -2,4 +2,5 @@ class Urls {
   static const ROOT_URL = "https://amarroom.com/api/v2/";
   static const POPULAR_DESTINATION = "${ROOT_URL}popular-destinations/";
   static const HOTEL_SAMPLE_LIST = "${ROOT_URL}search/filtered/?location=2&checkin=24-03-2020&checkout=25-03-2020&adults=2&rooms=1&page=";
+  static const ADD_NEW_URL = "${ROOT_URL}search/filtered/?location=2&checkin=24-03-2020&checkout=25-03-2020&adults=2&rooms=1&page=";
 }
